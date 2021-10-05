fbf31527602171d426ed2a8cfa34ca637eaa0137
Dima Po <dima.djunaev@gmail.com>
2021-10-05 17:49:25 +0500

    Header Mobile Optimization

--- src/views/modules/header/header.js
+++ src/views/modules/header/header.js
@@ -3,6 +3,7 @@ const langDropdownMenu = document.querySelector('.lang__dropdown-menu');
 const searchForm = document.querySelector('.search__form');
 const searchFormOpenBtn = document.querySelector('.search__open');
 const searchFormCloseBtn = document.querySelector('.search__input-icon--close');
+const mobileNav = document.querySelector('.header__mobile-nav');
 
 langCurrentBtn.addEventListener('click', (e) => {
   langDropdownMenu.classList.toggle('lang__dropdown-menu--active');
@@ -14,4 +15,8 @@ searchFormCloseBtn.addEventListener("click", () => {
 
 searchFormOpenBtn.addEventListener("click", () => {
   searchForm.classList.add("search__form--active");
+})
+
+mobileNav.addEventListener('click', () => {
+  mobileNav.classList.toggle('header__mobile-nav--active');
 })
