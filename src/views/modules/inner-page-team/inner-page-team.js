f2128ec154be513962fd1e1e35911c913486ff52
Dima Po <dima.djunaev@gmail.com>
2021-10-08 18:44:06 +0500

    Footer mobile optimization

--- src/views/modules/inner-page-team/inner-page-team.js
+++ src/views/modules/inner-page-team/inner-page-team.js
@@ -8,6 +8,7 @@ const closeSidebar = document.querySelector('.makro-team__sidebar-close');
 if(personCards && sidebar && sidebarOverlay && sidebarBody && closeSidebar) {
   personCards.forEach((elem) => {
     elem.addEventListener('click', (e) => {
+      document.body.setAttribute('style', 'overflow: hidden;');
       sidebar.classList.add('makro-team__sidebar--active');
       sidebarOverlay.classList.add('makro-team__sidebar-overlay--active');
       sidebarBody.classList.add('makro-team__sidebar-body--active');
@@ -15,12 +16,14 @@ if(personCards && sidebar && sidebarOverlay && sidebarBody && closeSidebar) {
   })
 
   closeSidebar.addEventListener('click', () => {
+    document.body.removeAttribute('style', 'overflow: hidden;');
     sidebar.classList.remove('makro-team__sidebar--active');
     sidebarOverlay.classList.remove('makro-team__sidebar-overlay--active');
     sidebarBody.classList.remove('makro-team__sidebar-body--active');
   })
 
   sidebarOverlay.addEventListener('click', () => {
+    document.body.removeAttribute('style', 'overflow: hidden;');
     sidebar.classList.remove('makro-team__sidebar--active');
     sidebarOverlay.classList.remove('makro-team__sidebar-overlay--active');
     sidebarBody.classList.remove('makro-team__sidebar-body--active');
