12b739775118e76047acefe1392d6454bdccc8a8
unknown <dima.djunaev@gmail.com>
2021-10-13 01:49:29 +0500

    Team Mobile

--- src/views/modules/inner-team/inner-team.js
+++ src/views/modules/inner-team/inner-team.js
@@ -1,6 +1,24 @@
-const card = document.querySelector('.makro-team__card');
+const cards = document.querySelectorAll('.makro-team__card');
+const cardClose = document.querySelectorAll('.makro-team__card-details-close');
 const cardDetails = document.querySelector('.makro-team__card-details');
 
-if (cardDetails) {
-  // cardDetails.setAttribute('style', `width:${card.offsetWidth * 3}px`)
+if (cards) {
+
+  const removeActiveCardClass = () => {
+    cards.forEach((elem) => {
+      elem.classList.remove('makro-team__card--active');
+    })
+  }
+
+  cards.forEach((card) => {
+    card.addEventListener('click', () => {
+      cardDetails.classList.add('makro-team__card-details--active');
+    })
+  })
+
+  cardClose.forEach((closeBtn) => {
+    closeBtn.addEventListener('click', () => {
+      cardDetails.classList.remove('makro-team__card-details--active');
+    })
+  })
 }
